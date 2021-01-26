ab8494b297df817bdda7e1d5106ffd3913c13555
Vincent Dharma Simanjaya <vincent.simanjaya@gmail.com>
2021-01-26 17:27:37 +0700

    update react native test renderer

--- src/scenes/ProductCollection/components/ProductsView.tsx
+++ src/scenes/ProductCollection/components/ProductsView.tsx
@@ -21,9 +21,9 @@ type SortProps = {
 };
 
 type FilterProps = {
-  priceRange: [number, number];
+  priceRange: Array<number>;
   onClearFilter: () => void;
-  onSetFilter: (values: [number, number]) => void;
+  onSetFilter: (values: Array<number>) => void;
   onValuesChangeStart: () => void;
   onValuesChangeFinish: () => void;
 };
