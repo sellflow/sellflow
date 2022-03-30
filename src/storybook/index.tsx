139ca9b56aeabff6333bfcea5bce50e89a5832a1
Kenny Cornelius <49273382+knycornelius@users.noreply.github.com>
2022-03-30 10:54:44 +0700

    [Chore] Upgrade Storybook (#260)
    Co-authored-by: Jonathan Lie <jonathanlie@Jonathans-MacBook-Air.local>

--- src/storybook/index.tsx
+++ src/storybook/index.tsx
@@ -2,8 +2,10 @@ import '../polyfills';
 import '../filterWarnings';
 
 import { registerRootComponent } from 'expo';
+import { useFonts } from 'expo-font';
 import React from 'react';
-import { Provider } from 'react-native-paper';
+import { ActivityIndicator, Provider } from 'react-native-paper';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import { configure, getStorybookUI } from '@storybook/react-native';
 
@@ -16,10 +18,21 @@ configure(() => {
 const StorybookUIRoot = getStorybookUI({
   onDeviceUI: true,
   disableWebsockets: true,
-  asyncStorage: require('@react-native-async-storage/async-storage') || null,
+  asyncStorage: AsyncStorage || null,
 });
 
 function App() {
+  let [fontsLoaded] = useFonts({
+    'SourceSansPro-Regular': require('../../assets/fonts/SourceSansPro-Regular.ttf'),
+    'SourceSansPro-Italic': require('../../assets/fonts/SourceSansPro-Italic.ttf'),
+    'SourceSansPro-Bold': require('../../assets/fonts/SourceSansPro-Bold.ttf'),
+    'SourceSansPro-SemiBold': require('../../assets/fonts/SourceSansPro-SemiBold.ttf'),
+  });
+
+  if (!fontsLoaded) {
+    return <ActivityIndicator />;
+  }
+
   return (
     <Provider theme={CustomTheme}>
       <StorybookUIRoot />
