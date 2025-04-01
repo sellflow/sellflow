7b2b9be82d15f0388c722e378c1b9b8c3b90b291
echo <vincentgarciajr2712@gmail.com>
2025-04-01 14:23:45 -0700

    [Feat] Implemented basic account page

--- app/(tabs)/account.tsx
+++ app/(tabs)/account.tsx
@@ -1,9 +1,73 @@
-import { Text, View } from "react-native";
+import { getAccessToken } from "@/lib/tokens";
+import { getUserInfo } from "@/shopify/user";
+import { Image } from "expo-image";
+import { useEffect, useState } from "react";
+import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
 
 export default function Account() {
-  return (
-    <View>
-      <Text>Account page</Text>
+  const [user, setUser] = useState();
+  const [loading, setLoading] = useState<boolean>();
+  useEffect(() => {
+    setLoading(true);
+    getAccessToken().then((accessToken) => {
+      if (accessToken) {
+        getUserInfo(accessToken).then(async (res) => {
+          const data = await res.json();
+          setUser(await data.data.customer);
+          setLoading(false);
+          console.log(user);
+        });
+      }
+    });
+  }, []);
+  return loading ? (
+    <ActivityIndicator color="#fff" />
+  ) : (
+    <View style={styles.Container}>
+      <Image style={styles.ProfilePicture} source={{ uri: user?.imageUrl }} />
+      <View>
+        <Text style={styles.UserInfoHeading}>First Name: </Text>
+        <Text style={styles.UserInfo}>{user?.firstName}</Text>
+      </View>
+      <View>
+        <Text style={styles.UserInfoHeading}>Last Name: </Text>
+        <Text style={styles.UserInfo}>{user?.lastName}</Text>
+      </View>
+      <View>
+        <Text style={styles.UserInfoHeading}>Phone Number: </Text>
+        <Text style={styles.UserInfo}>{user?.phoneNumber?.phoneNumber}</Text>
+      </View>
+      <View>
+        <Text style={styles.UserInfoHeading}>Email Address:</Text>
+        <Text style={styles.UserInfo}>{user?.emailAddress?.emailAddress}</Text>
+      </View>
+      <View>
+        <Text style={styles.UserInfoHeading}>Default Address: </Text>
+        <Text style={styles?.UserInfo}>{user?.defaultAddress?.address1}</Text>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  Container: {
+    maxWidth: 640,
+    width: "100%",
+    alignSelf: "center",
+    gap: 24,
+  },
+  ProfilePicture: {
+    alignSelf: "center",
+    width: 100,
+    height: 100,
+  },
+  UserInfoHeading: {
+    color: "white",
+    fontSize: 18,
+    marginBottom: 4,
+  },
+  UserInfo: {
+    color: "white",
+    fontWeight: 600,
+  },
+});
--- shopify/user.ts
+++ shopify/user.ts
@@ -1,5 +1,35 @@
-import { client } from "./client";
-
+// Retrieves user account info given user accessToken
+export const getUserInfo = async (accessToken: string) =>
+  await fetch(process.env.EXPO_PUBLIC_CUSTOMER_STORE_ENDPOINT!, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: accessToken,
+    },
+    body: JSON.stringify({
+      query: `
+    {
+      customer {
+        firstName
+        lastName
+        phoneNumber {
+          marketingState
+          phoneNumber
+        }
+        imageUrl
+        emailAddress {
+          marketingState
+          emailAddress
+        }
+        defaultAddress {
+          address1
+          address2
+        }
+      }
+    }
+    `,
+    }),
+  });
 // Retrieves user info given user accessToken
 export const getUser = async (accessToken: string) =>
   await fetch(process.env.EXPO_PUBLIC_CUSTOMER_STORE_ENDPOINT!, {
